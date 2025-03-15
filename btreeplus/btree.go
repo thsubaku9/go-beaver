6f5a22aa0a60c174f80e3a719828e9d5d2b45bf2
kaustubh <kaustubhkj@gmail.com>
2025-03-15 10:32:56 +0530

    basic mmap fn

--- btreeplus/btree.go
+++ btreeplus/btree.go
@@ -15,6 +15,16 @@ type BTree struct {
 	del func(uint64)       // deallocate a page number
 }
 
+func NewBTree(get func(uint64) BNode,
+	new func(BNode) uint64,
+	del func(uint64)) BTree {
+	return BTree{
+		get: get,
+		new: new,
+		del: del,
+	}
+}
+
 func checkLimit(key, val ByteArr) error {
 	if len(key) > BTREE_MAX_KEY_SIZE {
 		return fmt.Errorf("key limit exceeded")
--- kvstore/kv.go
+++ kvstore/kv.go
@@ -3,6 +3,7 @@ package kvstore
 import (
 	"beaver/btreeplus"
 	"beaver/helpers"
+	"fmt"
 	"syscall"
 )
 
@@ -10,11 +11,68 @@ type KV struct {
 	Path string
 	fd   int
 	tree btreeplus.BTree
+	mmap struct {
+		totalSize uint64
+		chunks    [][]byte
+	}
+	page struct {
+		flushedSize uint64
+		temp        []btreeplus.BNode
+	}
 	// todo more
 }
 
-func (db *KV) Open() error {
+func extendMmap(db *KV, size uint64) error {
+	if size <= db.mmap.totalSize {
+		return nil
+	}
+
+	incrementSize := max(db.mmap.totalSize, 64<<10)
+
+	for db.mmap.totalSize+incrementSize < size {
+		incrementSize += incrementSize
+	}
+
+	chunk, err := syscall.Mmap(db.fd, int64(db.mmap.totalSize), int(incrementSize),
+		syscall.PROT_READ, syscall.MAP_SHARED) // ro
+
+	if err != nil {
+		return fmt.Errorf("mmap :%w", err)
+	}
+
+	db.mmap.totalSize += incrementSize
+	db.mmap.chunks = append(db.mmap.chunks, chunk)
+
 	return nil
+}
+
+func (db *KV) Open() error {
+	db.tree = btreeplus.NewBTree(db.pageRead, db.pageAppend, db.pageDelete)
+	return nil
+}
+
+func (db *KV) pageRead(ptr uint64) btreeplus.BNode {
+	start := uint64(0)
+
+	for _, chunk := range db.mmap.chunks {
+		end := start + uint64(len(chunk))/btreeplus.BTREE_PAGE_SIZE
+		if ptr < end {
+			offset := btreeplus.BTREE_PAGE_SIZE * (ptr - start)
+			return chunk[offset : offset+btreeplus.BTREE_PAGE_SIZE]
+		}
+		start = end
+	}
+	panic("bad ptr")
+}
+
+func (db *KV) pageAppend(bnode btreeplus.BNode) uint64 {
+	ptr := db.page.flushedSize + uint64(len(db.page.temp))
+	db.page.temp = append(db.page.temp, bnode)
+	return ptr
+}
+
+func (db *KV) pageDelete(ptr uint64) {
+
 }
 
 func (db *KV) Get(key btreeplus.ByteArr) (val btreeplus.ByteArr, exists bool) {
@@ -47,6 +105,7 @@ func performFileUpdate(db *KV) error {
 }
 
 func writePages(db *KV) error {
+	// todo
 	return nil
 }
 
